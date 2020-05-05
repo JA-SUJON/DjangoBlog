24c9255894699f123d3d33a4506b2802720f0a94
jasujon <jubayedalamsujon@gmail.com>
2020-05-06 02:16:25 +0600

    create def __str__ function

--- blogApp/models.py
+++ blogApp/models.py
@@ -7,9 +7,17 @@ class Author(models.Model):
     name    = models.ForeignKey(User, on_delete=models.CASCADE)
     details = models.TextField()
 
+    def __str__(self):
+        return self.name.username
+    
+
 
 class Category(models.Model):
     name    = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+    
 
 
 class Article(models.Model):
@@ -17,4 +25,8 @@ class Article(models.Model):
     title          = models.CharField(max_length=200)
     body           = models.TextField()
     category       = models.ForeignKey(Category,on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
+    
 
