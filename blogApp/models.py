ea0e7e9e5898940d13c210db3f5f7596678ef187
jasujon <jubayedalamsujon@gmail.com>
2020-05-06 01:51:08 +0600

    Author Model Create

--- blogApp/models.py
+++ blogApp/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class Author(models.Model):
+    name    = models.ForeignKey(User, on_delete=models.CASCADE)
+    details = models.TextField()
+
