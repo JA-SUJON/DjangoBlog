e24bddf21c39dd492c20d7b99debcc3c0a346109
jasujon <jubayedalamsujon@gmail.com>
2020-05-06 01:57:14 +0600

    Category & Aritcle Model create & migrate

--- blogApp/models.py
+++ blogApp/models.py
@@ -7,3 +7,14 @@ class Author(models.Model):
     name    = models.ForeignKey(User, on_delete=models.CASCADE)
     details = models.TextField()
 
+
+class Category(models.Model):
+    name    = models.CharField(max_length=100)
+
+
+class Article(models.Model):
+    article_author = models.ForeignKey(Author,on_delete=models.CASCADE)
+    title          = models.CharField(max_length=200)
+    body           = models.TextField()
+    category       = models.ForeignKey(Category,on_delete=models.CASCADE)
+
