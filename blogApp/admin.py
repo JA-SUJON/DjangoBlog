e1e015d1cc1f7738faa54498392b88ea85b990a3
jasujon <jubayedalamsujon@gmail.com>
2020-05-06 23:45:12 +0600

    Add search field add in Author Model in admin.py->making authorModel class

--- blogApp/admin.py
+++ blogApp/admin.py
@@ -2,6 +2,11 @@ from django.contrib import admin
 from . models import Author,Category,Article
 # Register your models here.
 
-admin.site.register(Author)
+class authorModel(admin.ModelAdmin):
+    list_display  = ["__str__"]
+    search_fields = ["__str__","details"]
+    class Meta:
+        Model = Author
+admin.site.register(Author , authorModel)
 admin.site.register(Category)
 admin.site.register(Article)
