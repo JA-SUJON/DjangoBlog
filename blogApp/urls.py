b46cfe04143028c075ad09a1eba40a198e680835
jasujon <jubayedalamsujon@gmail.com>
2020-05-07 14:55:16 +0600

    Single.html & profile.html page mastering complite

--- blogApp/urls.py
+++ blogApp/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index , name="index"),
+    path('profile',views.profile , name="profile"),
+    path('article/<id>',views.article , name="article"),
 ]
--- blogApp/views.py
+++ blogApp/views.py
@@ -4,3 +4,9 @@ from django.shortcuts import render,HttpResponse
 def index(request):
     #return HttpResponse("<h1>Hello World .. </h1>")
     return render(request,'index.html')
+
+def profile(request):
+    return render(request, 'profile.html')
+
+def article(request , id):
+    return render(request, "single.html")
